8a7e15a5c779ed030c4f3dfc62abf3cd2d175bef
Wei Gao <jky239@gmail.com>
2014-04-21 18:18:11 +0800

    add search apis

---
+++ test/search.js
@@ -0,0 +1,40 @@
+var assert = require('chai').assert;
+
+var search = require('../lib')(require('couch-db')('http://couch.cortex.dp'));
+
+describe('search.js', function() {
+    this.timeout(40000);
+
+    it('searchByName', function(done) {
+        search.searchByName(['cortex', 'angularjs'], function(err, rows) {
+            assert(rows && rows.length);
+            done(err);
+        });
+    });
+
+    it('searchByAuthor', function(done) {
+        search.searchByAuthor('cortex-admin', function(err, rows) {
+            assert(rows && rows.length);
+            done(err);
+        });
+    });
+
+
+    it('searchByKeyword', function(done) {
+        search.searchByKeyword('android', {
+            limit: 40
+        }, function(err, rows) {
+            assert(rows && rows.length);
+            done(err);
+        });
+    });
+
+    it.skip('searchByWord', function(done) {
+        search.searchByWord('cortex', {
+            limit: 40
+        }, function(err, rows) {
+            assert(rows && rows.length);
+            done(err);
+        });
+    });
+});
